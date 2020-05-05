7ab5ce328b08a460382b4d030cd213d65c3fe980
panica913@gmail.com <panica913@gmail.com>
2020-05-05 17:03:55 +0300

    доработка автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -100,5 +100,5 @@ gulp.task("refresh", function(done) {
   done();
 });
 
-gulp.task("build", gulp.series("clean", "sprite", "images", "webp", "copy", "css", "html"));
+gulp.task("build", gulp.series("clean", "images", "webp", "sprite", "copy", "css", "html"));
 gulp.task("start", gulp.series("build", "server"));
